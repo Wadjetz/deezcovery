f53423f00d8d83fe077fe211d884373e3d7dc4e2
Wadjetz <egor.neon@gmail.com>
2015-02-14 19:32:57 +0100

    Add CoreData

---
+++ Deezcovery/Track.h
@@ -0,0 +1,19 @@
+//
+//  Track.h
+//  Deezcovery
+//
+//  Created by Egor Berezovskiy on 14/02/15.
+//  Copyright (c) 2015 ESGIAL1-2014. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+
+
+@interface Track : NSManagedObject
+
+@property (nonatomic, retain) NSNumber * track_id;
+@property (nonatomic, retain) NSString * name;
+@property (nonatomic, retain) NSString * path;
+
+@end
---
+++ Deezcovery/Track.m
@@ -0,0 +1,18 @@
+//
+//  Track.m
+//  Deezcovery
+//
+//  Created by Egor Berezovskiy on 14/02/15.
+//  Copyright (c) 2015 ESGIAL1-2014. All rights reserved.
+//
+
+#import "Track.h"
+
+
+@implementation Track
+
+@dynamic track_id;
+@dynamic name;
+@dynamic path;
+
+@end
