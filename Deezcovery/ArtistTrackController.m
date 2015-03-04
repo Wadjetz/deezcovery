b53bd4a5f00875b240644ac628d8894c96d2ef5e
Vuzi <vuzi@Vuzis-Mac.local>
2015-03-04 23:38:41 +0100

    Fix track image loading

--- Deezcovery/ArtistTrackController.m
+++ Deezcovery/ArtistTrackController.m
@@ -31,17 +31,19 @@
     CustomTrackCell *cell = [tableView dequeueReusableCellWithIdentifier:CUSTOM_TRACK_CELL];
     
     if (cell == nil) {
-        //cell = [[CustomTrackCell alloc] init]; // or your custom initialization
         cell = [[CustomTrackCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CUSTOM_TRACK_CELL];
     }
-    
-    
     
     Track *track = self.tracks[indexPath.row];
     
     dispatch_async(dispatch_get_global_queue(0, 0), ^{
         NSData *data = [[NSData alloc] initWithContentsOfURL:[NSURL URLWithString:track.image]];
-        [cell.customTrackImage setImage:[[UIImage alloc] initWithData:data scale:2.0]];
+        
+        dispatch_async(dispatch_get_main_queue(), ^{
+            // Update image
+            [cell.customTrackImage setImage:[[UIImage alloc] initWithData:data scale:2.0]];
+        });
+        
     });
 
     // Configure cell
