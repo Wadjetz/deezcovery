9f3db5d82b96c33ecea4444f356ef2aaa805e3cb
Wadjetz <egor.neon@gmail.com>
2015-03-05 13:10:42 +0100

    Fix

--- Deezcovery/ArtistTrackController.m
+++ Deezcovery/ArtistTrackController.m
@@ -36,17 +36,18 @@
     
     Track *track = self.tracks[indexPath.row];
     
-    if (self.offline == NO) {
-        dispatch_async(dispatch_get_global_queue(0, 0), ^{
-            NSData *data = [[NSData alloc] initWithContentsOfURL:[NSURL URLWithString:track.image]];
-            
+    dispatch_async(dispatch_get_global_queue(0, 0), ^{
+        NSError * error = nil;
+        NSData *data = [[NSData alloc] initWithContentsOfURL:[NSURL URLWithString:track.image] options:0 error:&error];
+        //NSLog(@"Error = %@", error);
+        if (error == nil) {
             dispatch_async(dispatch_get_main_queue(), ^{
                 // Update image
                 [cell.customTrackImage setImage:[[UIImage alloc] initWithData:data scale:2.0]];
             });
-            
-        });
-    }
+        }
+        
+    });
 
     // Configure cell
     //cell.textLabel.text = [[NSString alloc] initWithFormat:@"%@ - (%@)", track[@"title"], track[@"album"][@"title"]];
